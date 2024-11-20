144a147fa44d77618f733ff21180fbe01085074f
Hussein Tadicha <husseintadicha@Cexs-MacBook-Pro.local>
2024-11-20 21:32:23 +0000

    created products template and fields

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,9 @@
 """boutique_ado URL Configuration
 This is the root urls and we don't need include and 
 """
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name='home')
+    path('', views.index, name='home')
 ]
